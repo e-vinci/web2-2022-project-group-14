c51384df9c9d53c9b9ff86408888dcd2e477f62b
ThomasJohnen <thomas.johnen@hotmail.be>
2022-11-25 10:06:47 +0100

    add : adding modal for register and Login

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -2,6 +2,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../stylesheets/main.css';
 
 const homePage = `
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- login modal -->
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       MOTHER FUCKERS ! 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- register modal -->
+
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       FATHER SUCKERS ! 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <div class="d-flex justify-content-between">
         <div class="col-2 colonneLeft">
           <div class="shadow mb-5 bg-body rounded" id="innerColLeft">
@@ -22,7 +68,7 @@ const homePage = `
             <p>Tâche n°15</p>
           </div>
         </div>
-        <div class="col-8 colonne">
+        <div class="col-7 colonne">
           <div class="shadow p-3 mb-5 bg-body rounded">
             <p>Tâche n°1</p>
           </div>
@@ -31,7 +77,7 @@ const homePage = `
           </div>
         </div>
         <div class="col-2 colonneRight">
-          <div class="col-2 shadow mb-5 bg-body rounded" id="innerColRight">
+          <div class="shadow mb-5 bg-body rounded" id="innerColRight">
             <p>Ennemi n°1</p>
             <p>Ennemi n°2</p>
             <p>Ennemi n°3</p>  
