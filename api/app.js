e2572d5c0052c88dcb36e638e76a5302959c7eee
Lucas <lucas.fuentes@student.vinci.be>
2022-12-11 14:05:02 +0100

    remove ennemies routes into app.js

--- api/app.js
+++ api/app.js
@@ -5,12 +5,8 @@ const logger = require('morgan');
 const tacheRouter = require('./routes/taches');
 const pizzaRouter = require('./routes/pizzas');
 const authsRouter = require('./routes/auths');
-<<<<<<< HEAD
-
-=======
-const enemiesRouter = require('./routes/enemies');
 const playerCharacterRouter = require('./routes/playerCharacters');
->>>>>>> 10b0c2631de1ed17110d5a442397b76ba1ba96c3
+
 
 const app = express();
 
@@ -22,11 +18,7 @@ app.use(cookieParser());
 app.use('/pizzas', pizzaRouter);
 app.use('/taches', tacheRouter);
 app.use('/auths', authsRouter);
-<<<<<<< HEAD
-
-=======
-app.use('/enemies', enemiesRouter);
 app.use('/playerCharacters', playerCharacterRouter);
->>>>>>> 10b0c2631de1ed17110d5a442397b76ba1ba96c3
+
 
 module.exports = app;
